f4e51f380263397aa5747281769239444e232581
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2022-03-03 02:10:34 +0200

    01

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props:MessageType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <img src={props.avatar} alt={'ava'} className={s.avatar}/>
+            <div className={s.angle}/>
+            <div className={s.content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
