3d97535ce9c4134390a10e7ce3c68e5ec98cb9d1
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2022-05-23 16:44:28 +0300

    8 hw

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,8 +1,13 @@
 import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './HW8.module.css'
 
-// export type UserType =
+export type UserType = {
+    _id: number
+    name: string
+    age: number
+}
 
 const initialPeople = [
     {_id: 0, name: 'Кот', age: 3},
@@ -14,31 +19,33 @@ const initialPeople = [
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+    const [people, setPeople] = useState<Array<UserType>>(initialPeople)
 
-    // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
+    const finalPeople = people.map((p: UserType) => (
+        <div key={p._id} className={s.item}>
+            <span>{p.name}</span>
+            <span>{p.age}</span>
         </div>
     ))
 
     const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+    const check18 = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 18 }))
 
     return (
         <div>
             <hr/>
-            homeworks 8
+            <h3>Homework 8</h3>
 
-            {/*should work (должно работать)*/}
             {finalPeople}
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+            <div>
+                <SuperButton onClick={sortUp}>sort up</SuperButton>
+                <SuperButton onClick={sortDown}>sort down</SuperButton>
+                <SuperButton onClick={check18}> check 18</SuperButton>
+            </div>
 
             <hr/>
-            {/*для личного творчества, могу проверить*/}
             {/*<AlternativePeople/>*/}
             <hr/>
         </div>
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,21 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+export type ActionTypes = { type: 'sort', payload: 'up' | 'down' } | { type: 'check', payload: number }
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionTypes): Array<UserType> => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(a => a.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
